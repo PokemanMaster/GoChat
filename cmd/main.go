37994ef1b4e2f86846f40257d60b4776e7cdffde
28236 <2823631395@qq.com>
2024-11-19 14:15:01 +0800

    解决代码冲突

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,12 @@ package main
 import (
 	"IMProject/common"
 	"IMProject/router"
-<<<<<<< HEAD
 	"fmt"
 )
 
 func main() {
-	common.Init() // 初始化配置
-	fmt.Println("123121221233")
-=======
-)
-
-func main() {
-	common.Init()         // 初始化配置
->>>>>>> 654d6dd65ef53e6a8b34a0b7d48c3276960bbc4c
-	r := router.Router()  // 初始化路由
-	panic(r.Run(":9000")) // 初始化监听端口
+	common.Init()               // 初始化配置
+	fmt.Println("123121221233") // 打印测试信息
+	r := router.Router()        // 初始化路由
+	panic(r.Run(":9000"))       // 初始化监听端口
 }
