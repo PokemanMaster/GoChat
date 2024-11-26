26688d3d691a063584dca321a2472b6392077e93
28236 <2823631395@qq.com>
2024-11-26 10:31:15 +0800

    修改代码格式

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/PokemanMaster/GoChat/server/common"
 	"github.com/PokemanMaster/GoChat/server/router"
 )
 
 func main() {
-	common.Init()         // 初始化配置
-	r := router.Router()  // 初始化路由
+	common.Init()        // 初始化配置
+	r := router.Router() // 初始化路由
+	fmt.Println("")
 	panic(r.Run(":9000")) // 初始化监听端口
+
 }
