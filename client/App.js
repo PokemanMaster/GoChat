58fc018503ced16c2532d74941806279245fc624
28236 <2823631395@qq.com>
2024-11-26 10:34:43 +0800

    修改代码格式

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import {useRoutes} from "react-router-dom"
+import router from "../../../../ECP电商商城项目/Golang-Project/client/src/router";
+import React from 'react'
+
+
+export default function App() {
+    const tourist = useRoutes(router)
+    return (
+        <>
+            {tourist}
+        </>
+    )
+}
+
+
